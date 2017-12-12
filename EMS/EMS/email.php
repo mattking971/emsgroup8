104af312ef45ca2e0f881477a37d166420df2c61
richie541121 <richie.mccollom@gmail.com>
2017-12-12 16:40:55 -0600

    Add files via upload

---
+++ EMS/EMS/email.php
@@ -0,0 +1,4 @@
+<?php
+    mail('mattking971@yahoo.com', $_POST['PatientList"], $_POST['comment'], $_POST['serviceCost'], $_POST['insurance'], $_POST['output']);
+    ?>
+<p>Above is outlining your visit today. Please make sure to come by the office to pay the subtotal in cash or check. Thank You</p>
